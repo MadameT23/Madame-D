6c817b7f2ecaa9130a3d7262dd8442aae0d0042b
MadameT23 <131755298+MadameT23@users.noreply.github.com>
2023-05-02 09:26:15 +0200

    Erster Upload

---
+++ futterautomat.py
@@ -0,0 +1,27 @@
+
+
+print("****************************************\n")
+print("        Futter für den Streichelzoo\n          ")
+print("****************************************\n")
+
+print("Herzlich Willkommen!\n")
+print("Wir bieten Ihnen folgende Futtersorten an:\n")
+print("1 - Möhren")
+print("2 - Nüsse")
+print()
+
+while True:
+            futter= 0
+            futter = input("Welches Futter wünschen Sie? Sie können wählen zwischen Möhren (1) oder Nüssen (2).")
+
+            if futter == "1":
+                print("Sie haben Möhren ausgewählt. Damit können Sie unsere Pferde, Esel und Kaninchen füttern. Bitte passen Sie beim Füttern auf Ihre Hände auf!\nVielen Dank für Ihren Einkauf und viel Spaß in unserem wunderschönen Zoo!")
+
+            elif futter == "2":
+                print("Sie haben Nüsse ausgewählt. Damit können Sie unsere Kaninchen, Schildkröten und Vögel füttern. Bitte passen Sie beim Füttern auf Ihre Hände auf!n\Vielen Dank für Ihren Einkauf und viel Spaß in unserem wunderschönen Zoo!")
+    
+            else:
+                print("Ungültige Eingabe, bitte wählen Sie zwischen Möhren (1) und Nüssen (2).")
+                
+
+
